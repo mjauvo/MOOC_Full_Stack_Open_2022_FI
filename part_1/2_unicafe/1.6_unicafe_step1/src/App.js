ca6e5b72a47b9422ad0bb80eaf6595be55ec9b40
Markus Auvo <markus.auvo@helsinki.fi>
2022-06-05 23:15:38 +0300

    1.6_unicafe_step1

---
+++ part_1/2_unicafe/1.6_unicafe_step1/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Title = ({name}) => {
+    return (
+        <>
+            <h1>{name}</h1>
+        </>
+    )
+}
+
+const Button = ({behavior, value}) => {
+    return (
+        <>
+            <button onClick={behavior}>{value}</button>
+        </>
+    )
+}
+
+const Result = ({name, value}) => {
+    return (
+        <>
+            {name}: {value}<br/>
+        </>
+    )
+}
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGood = () => setGood(good+1)
+    const handleNeutral = () => setNeutral(neutral+1)
+    const handleBad = () => setBad(bad+1)
+  
+    return (
+        <div>
+            <Title name="Give Feedback"/>
+            <Button behavior={handleGood} value="GOOD" />
+            <Button behavior={handleNeutral} value="NEUTRAL" />
+            <Button behavior={handleBad} value="BAD" />
+
+            <Title name="Statistics"/>
+            <Result name="Good" value={good} />
+            <Result name="Neutral" value={neutral} />
+            <Result name="Bad" value={bad} />
+        </div>
+    )
+}
+
+export default App
