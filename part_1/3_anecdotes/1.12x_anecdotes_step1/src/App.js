45e7b0fa0a90242c760bde115a6edf80a2b39b7e
Markus Auvo <markus.auvo@helsinki.fi>
2022-06-07 00:08:13 +0300

    1.12*_anecdotes_step1

---
+++ part_1/3_anecdotes/1.12x_anecdotes_step1/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const randomize = (number) => (
+    Math.floor(Math.random() * number)
+)
+  
+const Button = ({handleClick, label}) => (
+    <div>
+        <button onClick={handleClick}>{label}</button>
+    </div>
+)
+  
+const App = () => {
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+    ]
+
+    const [selected, setSelected] = useState(0)
+
+    const handleSelect = () => setSelected(
+        randomize(anecdotes.length)
+    )
+    
+    return (
+        <div>
+            <Button handleClick = {handleSelect} label = "GET RANDOMIZED!" />
+            <p>{anecdotes[selected]}</p>
+        </div>
+      )
+}
+
+export default App
